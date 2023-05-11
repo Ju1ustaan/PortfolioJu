0f8e0efd53fe09665d4e6c4cca25531ca6f50b0d
luijulus <luijulus@gmail.com>
2023-05-11 15:14:34 +0600

    portfolio v2.0 media.css correct

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,21 +5,31 @@ import { NavLink } from 'react-router-dom'
 const Header = () => {
     return (
         <header className="header">
-        <div className="container">
-            <div className="header__wrapper">
-                <NavLink to="/" className="header__logo">Portfolio</NavLink>
-                 <nav className="header__nav">
-                    <NavLink to="/project" className="header__link">Проекты</NavLink>
-                    <NavLink to="/contact" className="header__link">Контакты</NavLink>
-                    <NavLink to="/about" className="header__link">О себе</NavLink>
-                </nav> 
-                <label id="switch" className="switch">
-                    <input type="checkbox" id="slider"/> 
-                    <span className="slider round"></span>
-                </label>
+            <div className="container">
+                <div className="header__wrapper">
+                    <nav className="header__nav-media">
+                        <input type="checkbox" id="menu-toggle" />
+                        <label id="trigger" for="menu-toggle"></label>
+                        <label id="burger" for="menu-toggle"></label>
+                        <ul id="menu" className='menu'>
+                            <li><NavLink className="header__link-media" to="/project">Проекты</NavLink></li>
+                            <li><NavLink className="header__link-media" to="/contact">Контакты</NavLink></li>
+                            <li><NavLink className="header__link-media" to="/about">О себе</NavLink></li>
+                        </ul>
+                    </nav>
+                    <NavLink to="/" className="header__logo">Portfolio</NavLink>
+                    <nav className="header__nav">
+                        <NavLink to="/project" className="header__link">Проекты</NavLink>
+                        <NavLink to="/contact" className="header__link">Контакты</NavLink>
+                        <NavLink to="/about" className="header__link">О себе</NavLink>
+                    </nav>
+                    <label id="switch" className="switch">
+                        <input type="checkbox" id="slider" />
+                        <span className="slider round"></span>
+                    </label>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
 
     )
 }
